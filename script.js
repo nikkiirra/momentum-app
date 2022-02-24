f4662538dd66a24bcf27654232322b11865db0a2
Nikki Sabenicio <nikkiirra@gmail.com>
2022-02-24 14:44:40 +0800

    added event listener click on submit name button

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // ----------------------- Main Page ----------------------- // 
+var enterName = document.getElementById("submitName")
+var inputName = document.getElementById("typeName")
+
+enterName.addEventListener("click", greet)
+inputName.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+      greet();
+  }
+})
