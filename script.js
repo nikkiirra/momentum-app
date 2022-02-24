06b58effe5a32c5383ae6fd78440cee4d3a76cef
Nikki Sabenicio <nikkiirra@gmail.com>
2022-02-24 15:09:26 +0800

    debugged submitName and inputName function, adding back the display none style

--- script.js
+++ script.js
@@ -1,14 +1,63 @@
+//----------To hide all elements prior to submitting name----------//
+var nameGreeting = document.getElementById("greetUser");
+var greeting = document.getElementById("greeting");
+greeting.style.display = "none";
+
+var askFocus = document.getElementById("inquireFocus");
+askFocus.style.display = "none";
+
+// var quote = document.getElementById("quote");
+// quote.style.display = "none";
+
+var mainFocus = document.getElementById("main-focus");
+mainFocus.style.display = "none";
+
+
 // ----------------------- Main Page ----------------------- // 
-var enterName = document.getElementById("submitName")
-var inputName = document.getElementById("typeName")
 
-enterName.addEventListener("click", greet)
+var submitName = document.getElementById("submitName");
+var inputName = document.getElementById("typeName");
+submitName.addEventListener("click", greet);
 inputName.addEventListener('keypress', function (event) {
-  if (event.key === 'Enter') {
-      greet()
-  }
-})
+    if (event.key === 'Enter') {
+        greet();
+    }
+});
 
+
+// ----------------------- User Greeting - Main Page Display ----------------------- // 
+var greeting = document.getElementById("greeting");
+var askName = document.getElementById("nameData");
+function greet() {
+    username = inputName.value;
+    askName.style.display = "none";
+    greeting.style.display = "flex";
+    askFocus.style.display = "flex";
+    renderQuote();
+   
+}
+
+// ----------------------- TIME and Greeting Setup ----------------------- // 
+function updateTime() {
+    
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var clockView = currentTime.toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        minute:'2-digit'
+      });
+    
+    document.getElementById("timeDisplay").innerHTML = clockView;
+    if (hours < 12) {
+        nameGreeting.textContent = `Good Morning, ${username}.`;
+    } else if (hours >= 12 && hours <= 17) {
+        nameGreeting.textContent = `Good Afternoon, ${username}.`;
+    } else {
+        nameGreeting.textContent = `Good Evening, ${username}.`;
+    }
+
+}
+setInterval(updateTime, 1000);
 
 // ----------------------- Main Focus ----------------------- // 
 var displayFocus = document.getElementById("display-focus");
@@ -26,53 +75,3 @@ inputFocus.addEventListener('keypress', function (event) {
         renderFocus();
     }
 });
-
-
-// ----------------------- User Greeting - Main Page Display ----------------------- // 
-var greeting = document.getElementById("greeting");
-var askName = document.getElementById("nameData");
-function greet() {
-    username = inputName.value;
-    askName.style.display = "none";
-    greeting.style.display = "flex";
-    askFocus.style.display = "flex";
-
-// ----------------------- TIME and Greeting Setup ----------------------- // 
-function updateTime() {
-    
-  var currentTime = new Date();
-  var hours = currentTime.getHours();
-  var clockView = currentTime.toLocaleTimeString(navigator.language, {
-      hour: '2-digit',
-      minute:'2-digit'
-    });
-  
-  document.getElementById("timeDisplay").innerHTML = clockView;
-  if (hours < 12) {
-      nameGreeting.textContent = `Good Morning, ${username}.`;
-  } else if (hours >= 12 && hours <= 17) {
-      nameGreeting.textContent = `Good Afternoon, ${username}.`;
-  } else {
-      nameGreeting.textContent = `Good Evening, ${username}.`;
-  }
-
-}
-setInterval(updateTime, 1000);
-
-
-
-
-//----------To hide all elements prior to submitting name----------//
-var nameGreeting = document.getElementById("greetUser");
-var greeting = document.getElementById("greeting");
-greeting.style.display = "none";
-
-
-var quote = document.getElementById("quote");
-quote.style.display = "none";
-
-var askFocus = document.getElementById("inquireFocus");
-askFocus.style.display = "none";
-
-var mainFocus = document.getElementById("main-focus");
-mainFocus.style.display = "none";
